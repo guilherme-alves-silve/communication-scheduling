c850d9fbc868a2c9c2960398a1185e73e37ee8d2
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2020-07-20 23:51:23 -0300

    Remove classe inutil

--- src/native-test/java/br/com/scheduling/NativecomunicacaoIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.scheduling;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativecomunicacaoIT extends agendamentoComunicacaoTest {
-
-    // Execute the same tests but in native mode.
-}
